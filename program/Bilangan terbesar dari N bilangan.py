873398d973ffed1d0cd72d6d76b817b6a2f7a466
Nomynameishaun <90388406+Nomynameishaun@users.noreply.github.com>
2024-10-22 22:53:51 +0700

    Update Bilangan terbesar dari N bilangan.py

--- program/Bilangan terbesar dari N bilangan.py
+++ program/Bilangan terbesar dari N bilangan.py
@@ -1,19 +1,12 @@
-# Memulai program
-bilangan_maksimal = float('-inf')  # Inisialisasi bilangan maksimal dengan negatif tak terhingga
-
-# Input jumlah bilangan yang akan dimasukkan
+bilangan_maksimal = float('-inf')  
 n = int(input("Masukkan jumlah bilangan: "))
 
-# Loop untuk meminta input bilangan sebanyak 'n' kali
 while n > 0:
     bilangan = int(input("Masukkan bilangan: "))
     
-    # Mengecek apakah bilangan lebih besar dari bilangan maksimal
     if bilangan > bilangan_maksimal:
-        bilangan_maksimal = bilangan  # Update bilangan maksimal
+        bilangan_maksimal = bilangan
         
-    # Mengurangi jumlah iterasi
     n -= 1
 
-# Output bilangan maksimal
 print("Bilangan maksimal adalah:", bilangan_maksimal)
